e0fe5fe8eabca0362b01935663ae1500c954d5d3
Osman <suulisindiyaka@gmail.com>
2020-04-11 23:51:27 -0700

    Add audit logs

--- account/accountController.js
+++ account/accountController.js
@@ -3,12 +3,12 @@ const accountValidator = require("./accountValidator");
 const accountRepository = require("./accountRepository");
 const modelBuilder = require("../core/modelBuilder");
 
-module.exports = function accountModule({ app, httpRequestAdaptor }) {
-  accountService = makeService({
-    accountRepository,
-    modelBuilder,
-    accountValidator
-  });
+function AccounController() {
+  
+}
+
+module.exports = function accountModule({ app, appConfig,appAuditLog, httpRequestAdaptor }) {
+  accountService = makeService({accountRepository, modelBuilder,accountValidator});
 
   app.post("/api/accounts", httpRequestAdaptor(accountService.createAccount));
   //   app.get("/api/accounts", httpRequestAdaptor(accountService.getAccounts));
--- auditLogs/auditLog.js
+++ auditLogs/auditLog.js
@@ -1,5 +1,50 @@
-function makeAuditLogger(db) {
-    return async function log(message) {
-        db.insert(message);
+function makeAuditLogger(auditLogDb) {
+    const db = auditLogDb();
+    const that = this;
+
+    return function log(func, params) {
+        return new Promise(handleLogging(func, params)).finally(performLoging());
+    }
+
+
+    async function performLoging(func, params) {
+        return () => {
+            try {
+                const funcName = getFuncName(func)
+                const loggerInput = {
+                    methodName:funcName,
+                    clientIpAddress:params.ip,
+                    browserInfo:'',
+                    executionDuration:that.elapse,
+                    exception:that.error,
+                    parameters:params,
+                    returnValues:that.result,
+                    requestUrl:params.path,
+                }
+                await db.insert(loggerInput);
+            } catch (err) {
+
+            }
+        };
+    }
+
+    async function handleLogging(func, params) {
+        return (resolve, reject) => {
+            try {
+                const funcStart = new Date();
+                const result = await func(params);
+                const funcEnd = new Date();
+                that.result = result;
+                that.elapse = funcEnd - funcStart;
+                return resolve(result);
+            } catch (err) {
+                that.error=err;
+                return reject(err);
+            }
+        };
+    }
+
+    function getFuncName(func) {
+        return func.name;
     }
 }

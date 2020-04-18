132e4027442960a2e66fcf0235b283b118f13316
Obed <obedamoasi@gmail.com>
2020-04-18 22:53:15 +0000

    refactor: add default params to makeAccountCreator

--- account/usecase/openAccount.js
+++ account/usecase/openAccount.js
@@ -1,37 +1,60 @@
-const _AccountDb=require('../accountRepository');
-const _modelBuilder=require('../../core/modelBuilder');
-const _execute=require('../../core/execute');
-const _createCustomer=require('../../customer/usecase/createCustomer');
+const _AccountDb = require('../accountRepository');
+const _modelBuilder = require('../../core/modelBuilder');
+const _execute = require('../../core/execute');
+const _createCustomer = require('../../customer/usecase/createCustomer');
 
 
-module.exports = function makeAccountCreator({AccountDb,modelBuilder,createCustomer,execute,appEvent}={}) {
-  const db = AccountDb? new AccountDb():new _AccountDb();
-  buildModel=modelBuilder||_modelBuilder;
-  execute=execute||_execute;
-  createCustomer=createCustomer? createCustomer():  _createCustomer();
-  
-  return function openAccount({ body, appSession }={}) {
-    const {account,customer}=body;
-    return execute(async ()=>{
-      const createdCustomer=await createCustomer(customer,appSession);
+module.exports = function makeAccountCreator({
+  AccountDb,
+  modelBuilder,
+  createCustomer,
+  execute,
+  appEvent
+} = {
+  AccountDb: _AccountDb,
+  modelBuilder: _modelBuilder,
+  execute: _execute,
+  createCustomer: _createCustomer
+}) {
+  const db = new AccountDb();
+  buildModel = modelBuilder;
+  createCustomer = createCustomer();
 
-      const accountModel = buildModel({model:account,modelValidator:validateCreateAccountModel,appSession});
-      accountModel.customerId=createdCustomer._id;
-      const savedAccount=await db.insert(accountModel);
+  return function openAccount({
+    body,
+    appSession
+  } = {}) {
+    const {
+      account,
+      customer
+    } = body;
+    return execute(async () => {
+      const createdCustomer = await createCustomer(customer, appSession);
 
-      appEvent.trigger('account-opened',{customerId:createdCustomer._id,accountId:savedAccount._id});
+      const accountModel = buildModel({
+        model: account,
+        modelValidator: validateCreateAccountModel,
+        appSession
+      });
+      accountModel.customerId = createdCustomer._id;
+      const savedAccount = await db.insert(accountModel);
+
+      appEvent.trigger('account-opened', {
+        customerId: createdCustomer._id,
+        accountId: savedAccount._id
+      });
 
       return savedAccount;
     });
-   
+
   };
 
   function validateCreateAccountModel(createAccountModel) {
     return {
-      hasError:undefined,
-      errorMessage:''
+      hasError: undefined,
+      errorMessage: ''
     }
   }
 
-  
+
 };
